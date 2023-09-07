b37a9e510822bde4f907e7c20eee78d1ca10cb10
aristata <aristatait@gmail.com>
2023-09-07 17:16:20 +0900

    feat: 컨텐츠 레이어 세팅 및 예시 포스트 작성

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,23 @@
+import PostCard from "components/PostCard"
+import { allPosts } from "contentlayer/generated"
+import { compareDesc } from "date-fns"
+
 const BlogPage = () => {
+  const posts = allPosts.sort((a, b) =>
+    compareDesc(new Date(a.date), new Date(b.date))
+  )
+
   return (
     <>
       <div>blog page</div>
+      <div className="mx-auto max-w-xl py-8">
+        <h1 className="mb-8 text-center text-2xl font-black">
+          Next.js + Contentlayer Example
+        </h1>
+        {posts.map((post, idx) => (
+          <PostCard key={idx} {...post} />
+        ))}
+      </div>
     </>
   )
 }
